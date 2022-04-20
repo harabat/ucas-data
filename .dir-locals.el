b44e805ce4493a8e5951de9790f69a773b00c264
soup <2-soup@users.noreply.gitlab.soupy-soup.org>
2022-04-20 16:19:00 +0200

    remove .dir_locals.el

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((ein:notebook-mode . ((eval . (conda-env-activate "data-science-exploratory")))))
-((ein:notebooklist-mode . ((eval . (conda-env-activate "data-science-exploratory")))))
-((poly-ein-mode . ((eval . (conda-env-activate "data-science-exploratory")))))
-((python-mode . ((eval . (conda-env-activate "data-science-exploratory")))))
